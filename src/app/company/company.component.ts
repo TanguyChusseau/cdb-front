1aa2d7c1194ef1c82be78f82b4d2cdfd5eefd804
TanguyChusseau <tchusseau@excilys.com>
2018-04-25 11:12:39 +0200

    [FEAT] (Angular-Front) Trying to make the app look clean

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Company } from '../model/company.model';
 import { CompanyService } from '../company.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-company',
@@ -13,16 +14,13 @@ export class CompanyComponent {
   @Input() company: Company;
   @Output() deletedCompany: EventEmitter<Company> = new EventEmitter();
 
-  constructor(private companyService: CompanyService) {
-  }
-
-  toggleExpand(): void {
-    this.isExpanded = !this.isExpanded;
+  constructor(private companyService: CompanyService, private router: Router) {
   }
 
   deleteCompany(): void {
     this.companyService.deleteCompany(this.company.id)
-      .subscribe(() => this.deletedCompany.emit(this.company));
+      .subscribe(() => this.router.navigate(['companies']),
+        error => console.error(error));
   }
 
 }
