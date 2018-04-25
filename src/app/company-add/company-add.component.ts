418ae8f8983f8908e18197aa5e8a351ddebd4942
TanguyChusseau <tchusseau@excilys.com>
2018-04-25 16:40:16 +0200

    [FEAT] (Front) Adding icons

--- src/app/company-add/company-add.component.ts
+++ src/app/company-add/company-add.component.ts
@@ -11,38 +11,25 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 })
 export class CompanyAddComponent implements OnInit {
 
-  //companyAddFormGroup: FormGroup;
+  companyAddFormGroup: FormGroup;
   company: Company = new Company();
   constructor(private companyService: CompanyService, private route: ActivatedRoute, private router: Router, private fb: FormBuilder) { }
 
   ngOnInit() {
-   //this.createForm();
-   //this.add();
+    this.createForm();
   }
 
-add() {
-  this.companyService.addCompany(this.company)
-      .subscribe(() => this.router.navigate(['companies']),
-          error => console.error(error));
-}
-
-/*
   submitMethod(): void{
-    const obj = this.companyAddFormGroup.value;
-    console.log(obj);
-    let newCompany: Company;
-    newCompany = obj as Company;
-    console.log(newCompany);
     this.companyService.addCompany(this.company)
       .subscribe(() => this.router.navigate(['companies']),
-          error => console.error(error));
+        error => console.error(error));
   }
 
   createForm() {
     this.companyAddFormGroup = this.fb.group({
       name: ['', Validators.required]
     });
-}
-*/
+  }
+
 
 }
